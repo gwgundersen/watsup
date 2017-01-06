ef8e1a5ce85a6d0d7788e35de71c29e58cd11bb5
Thomas Schaffner <tfs3@cs.princeton.edu>
2017-01-06 13:44:16 -0500

    Dummy status endpoint

--- watsup/endpoints/index.py
+++ watsup/endpoints/index.py
@@ -1,7 +1,7 @@
 """Serves index page.
 """
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 from watsup.config import config
 
@@ -19,3 +19,12 @@ def index_page():
     """
     users = mongo.db.users.find({})
     return render_template('index.html', data=list(users))
+
+
+@index.route('/status/', methods=['GET'])
+def watsup_status():
+	""" For now, always responds with "logged out"
+		Verifies that server is running WATSUP protocol
+	"""
+
+	return jsonify("Logged out"), 200
